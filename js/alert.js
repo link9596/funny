29b0867d8de673779eec16443e2dcc7c7066ef2f
Link <1316341442@qq.com>
2019-08-07 22:17:02 +0800

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -14,7 +14,7 @@ if( window.ActiveXObject ){
 
 }else{
     function addLink(){
-   document.getElementById('alert').style.top='0px';
+   document.getElementById('alert').style.top='100px';
    //document.getElementById('alert').style.WebkitTransform='translateY(80px)';
    document.getElementById('alert').innerHTML="复制成功！如引用请注明原作者及地址！";
     }
