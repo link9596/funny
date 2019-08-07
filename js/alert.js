85949efb91c732c590ced1f995de9e16b76e3a02
Link <1316341442@qq.com>
2019-08-07 21:52:03 +0800

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -14,7 +14,7 @@ if( window.ActiveXObject ){
 
 }else{
     function addLink(){
-   document.getElementById('alert').style.transform='translateY(100px)';
+   document.getElementById('alert').style.transform='translateY(77px)';
    document.getElementById('alert').style.WebkitTransform='translateY(100px)';
     }
     document.oncopy = addLink;
