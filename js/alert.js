cd1e0e39b39c9ce9def8bb4c83e0ef1b49159727
Link <1316341442@qq.com>
2019-08-07 21:36:56 +0800

    Create alert.js

---
+++ js/alert.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+  document.getElementById('alert').style.Transform='translateY(-100px)';
+  document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
+}
+
+window.setInterval(clean, 4000);
+
+function clean(){
+  document.getElementById('alert').style.transform='translateY(-100px)';
+  document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
+}
+
+if( window.ActiveXObject ){
+
+}else{
+    function addLink(){
+   document.getElementById('alert').style.transform='translateY(77px)';
+   document.getElementById('alert').style.WebkitTransform='translateY(78px)';
+    }
+    document.oncopy = addLink;
+}  
