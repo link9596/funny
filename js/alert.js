074273bf4c159e855941883c077725822f8f8706
Link <1316341442@qq.com>
2019-08-07 22:04:42 +0800

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,21 +1,21 @@
 window.onload=function(){
-  document.getElementById('alert').style.Transform='translateY(-100px)';
-  document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
+  document.getElementById('alert').style.top='-500px';
+  //document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
 }
 
 window.setInterval(clean, 8000);
 
 function clean(){
-  document.getElementById('alert').style.transform='translateY(-100px)';
-  document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
+  document.getElementById('alert').style.top='-500px';
+  //document.getElementById('alert').style.WebkitTransform='translateY(-100px)';
 }
 
 if( window.ActiveXObject ){
 
 }else{
     function addLink(){
-   document.getElementById('alert').style.transform='translateY(77px)';
-   document.getElementById('alert').style.WebkitTransform='translateY(80px)';
+   document.getElementById('alert').style.top='0px';
+   //document.getElementById('alert').style.WebkitTransform='translateY(80px)';
    document.getElementById('alert').innerHTML="复制成功！如引用请注明原作者及地址！";
     }
     document.oncopy = addLink;
